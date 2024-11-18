3a3e1354cfb3660526728b7d08a9d5dff86b0e7b
sachin-solankar <solankar.sbr@gmail.com>
2024-11-18 10:25:56 +0530

    Update app.component.ts
    Changes in this file - remove the comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'TaskManagement';
   
-  // ngOnInit(){
-  //   this.prime()
-  // }
   
-
-  // prime(){
-  //    var num:number=1000;
-  //    var count:number;
-  //    var rem:number=0;
-  //    var temp:number;
-  //    for(let i=1;i<=num;i++){
-
-  //       count=0;
-  //       temp=i;
-  //       while(temp>0){
-  //         rem=temp%10;
-  //         if(rem==1){
-  //           count++;
-  //         }
-  //         temp=temp/10;
-  //       }
-  //       if(count==1){
-  //         console.log(i);
-  //       }
-  //    }
-  // }
 }
